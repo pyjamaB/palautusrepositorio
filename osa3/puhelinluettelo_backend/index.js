174dfedd55a966678f6252761ad8ce357e4c623e
Tommi Koskinen <156694715+pyjamaB@users.noreply.github.com>
2026-08-29 09:58:49 +0300

    "Make changes to osa3"

--- osa3/puhelinluettelo_backend/index.js
+++ osa3/puhelinluettelo_backend/index.js
@@ -1,30 +1,11 @@
+require('dotenv').config()
 const express = require('express')
+const Person = require('./models/person')
 const app = express()
+
+let persons = []
 const morgan = require('morgan')
 const date = new Date()
-
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345"
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122"
-  }
-]
 
 
 app.use(morgan('tiny'))
@@ -39,7 +20,9 @@ app.get('/info', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response) => {
